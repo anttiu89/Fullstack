e382208eb40720f2043af5f12b8891c343591b7e
Antti Uusimäki <antti.uusimaki@etteplan.com>
2022-08-13 15:22:55 +0300

    1.5: kurssitiedot step5

--- osa1/kurssitiedot/src/App.js
+++ osa1/kurssitiedot/src/App.js
@@ -52,27 +52,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header header={course} />
-      <Content partObjectList={parts} />
-      <Total partObjectList={parts} />
+      <Header header={course.name} />
+      <Content partObjectList={course.parts} />
+      <Total partObjectList={course.parts} />
     </div>
   )
 }

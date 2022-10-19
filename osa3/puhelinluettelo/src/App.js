8f8ae7645c4623d5a55ac6d7c90af96173dfc148
Antti Uusimäki <antti.uusimaki@etteplan.com>
2022-10-19 16:26:15 +0300

    3.9 puhelinluettelon backend step9

---
+++ osa3/puhelinluettelo/src/App.js
@@ -0,0 +1,319 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import personService from './services/persons'
+
+const containsName = (paPersonArray, paName) => {
+  let nameArray = paPersonArray.map((person) => {
+    return person.name;
+  })
+  let containsVariable = nameArray.includes(paName)
+  return containsVariable
+}
+
+const getPerson = (paPersonArray, paName) => {
+  let personObject = {
+    name: "",
+    number: "",
+    id: -1
+  }
+
+  let filteredPersonArray = paPersonArray.filter((person) => {
+    return person.name === paName
+  })
+
+  if (filteredPersonArray.length > 0) {
+    personObject = {
+      name: filteredPersonArray[0].name,
+      number: filteredPersonArray[0].number,
+      id: filteredPersonArray[0].id
+    }
+  }
+
+  return personObject
+}
+
+const filterPersons = (paPersonArray, paFilter) => {
+  console.log(paPersonArray)
+  let filteredPersonArray = paPersonArray.filter((person) => {
+    if (paFilter.length === 0 || person.name.toUpperCase().includes(paFilter.toUpperCase()) === true) {
+      return true
+    }
+    else {
+      return false
+    }
+  })
+  console.log(filteredPersonArray.length)
+  return filteredPersonArray
+}
+
+const Person = (props) => {
+  console.log(props)
+  return (
+    <p>{props.person.name} {props.person.number} <Button onClick={props.handleRemoveClick} text={"delete"} value={props.person} /> </p>
+  )
+}
+
+const Persons = (props) => {
+  console.log("Persons props", props)
+  let filteredPersonArray = filterPersons(props.persons, props.filter)
+  return (
+    <div>
+      {filteredPersonArray.map(person =>
+        <Person key={person.name} person={person} handleRemoveClick={props.handleRemoveClick} />
+      )}
+    </div>
+  )
+}
+
+const Button = (props) => { 
+  console.log("Button", props)
+  return (
+    <button onClick={() => props.onClick(props.value)}>
+      {props.text}
+    </button>
+  )
+}
+
+const ButtonSubmit = (props) => { 
+  console.log(props)
+  return (
+    <div>
+      <button type={props.type}>
+        {props.text}
+      </button>
+    </div>
+  )
+}
+
+const FormAddPerson = (props) => { 
+  console.log(props)
+  return (
+    <form onSubmit={props.onSubmit}>
+      <Input text={"name: "} value={props.newNameValue} onChange={props.newNameOnChange} />
+      <Input text={"number: "} value={props.newNumberValue} onChange={props.newNumberOnChange} />
+      <ButtonSubmit type={"submit"} text={"add"} />
+    </form>
+  )
+}
+
+const Input = (props) => { 
+  console.log(props)
+  return (
+    <div>
+      {props.text} <input value={props.value} onChange={props.onChange} />
+    </div>
+  )
+}
+
+const Message = (props) => {
+  console.log(props)
+  if (props.message.message === null) {
+    return null
+  }
+
+  let classNameCss = "message"
+  if (props.message.isError === true) {
+    classNameCss = "errorMessage"
+  }
+  
+  return (
+    <div className={classNameCss}>
+      {props.message.message}
+    </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [newFilterByName, setNewFilterByName] = useState('')
+  const [message, setMessage] = useState({ message: null, isError: false })
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(allPersons => {
+        setPersons(allPersons)
+      })
+  }, [])
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    if (containsName(persons, newName) === true) {
+      let personObjectUpdate = getPerson(persons, newName)
+      if (personObjectUpdate.number !== newNumber) {
+        if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+          console.log("Update", newName, newNumber)
+          personObjectUpdate.number = newNumber
+          console.log("Update", personObjectUpdate)
+          updatePerson(personObjectUpdate)
+        }
+      }
+      else {
+        alert(`${newName} is already added to phonebook`)
+      }
+    }
+    else {
+      personService
+        .create(personObject)
+        .then(createdPerson => {
+          setPersons(persons.concat(createdPerson))
+          setNewName('')
+          setNewNumber('')
+          const newMessage = { 
+            message: `Added ${personObject.name}`, 
+            isError: false 
+          }
+          setMessage(newMessage)
+          setTimeout(() => {
+            const emptyMessage = { 
+              message: null, 
+              isError: false 
+            }
+            setMessage(emptyMessage)
+          }, 5000)
+        })
+        .catch(error => {
+          const newMessage = { 
+            message: `Error occurred.`, 
+            isError: true 
+          }
+          setMessage(newMessage)
+          setTimeout(() => {
+            const emptyMessage = { 
+              message: null, 
+              isError: false 
+            }
+            setMessage(emptyMessage)
+          }, 5000)
+        })
+    }
+  }
+
+  const updatePerson = (personObject) => {
+    personService
+      .update(personObject.id, personObject)
+      .then(updatedPerson => {
+        setPersons(persons.map((person) => {
+          if (person.id !== personObject.id) {
+            return person
+          }
+          else {
+            return updatedPerson
+          }
+        }))
+        const newMessage = { 
+          message: `Updated ${personObject.name}`, 
+          isError: false 
+        }
+        setMessage(newMessage)
+        setTimeout(() => {
+          const emptyMessage = { 
+            message: null, 
+            isError: false 
+          }
+          setMessage(emptyMessage)
+        }, 5000)
+      })
+      .catch(error => {
+        const newMessage = { 
+          message: `Information of ${personObject.name} has already been removed from server.`, 
+          isError: true 
+        }
+        setMessage(newMessage)
+        setTimeout(() => {
+          const emptyMessage = { 
+            message: null, 
+            isError: false 
+          }
+          setMessage(emptyMessage)
+        }, 5000)
+        setPersons(persons.filter(person => person.id !== personObject.id))
+      })
+  }
+
+  const removePerson = (personObject) => {
+    personService
+      .remove(personObject.id)
+      .then(() => {
+        setPersons(persons.filter((person) => {
+          if (person.id !== personObject.id) {
+            return true
+          }
+          else {
+            return false
+          }
+        }))
+        const newMessage = { 
+          message: `Deleted ${personObject.name}`, 
+          isError: false 
+        }
+        setMessage(newMessage)
+        setTimeout(() => {
+          const emptyMessage = { 
+            message: null, 
+            isError: false 
+          }
+          setMessage(emptyMessage)
+        }, 5000)
+      })
+      .catch(error => {
+        const newMessage = { 
+          message: `Information of ${personObject.name} has already been removed from server.`, 
+          isError: true 
+        }
+        setMessage(newMessage)
+        setTimeout(() => {
+          const emptyMessage = { 
+            message: null, 
+            isError: false 
+          }
+          setMessage(emptyMessage)
+        }, 5000)
+        setPersons(persons.filter(person => person.id !== personObject.id))
+      })
+  }
+
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterByNameChange = (event) => {
+    console.log(event.target.value)
+    setNewFilterByName(event.target.value)
+  }
+
+  const handleRemoveClick = (person) => {
+    if (window.confirm(`Delete ${person.name} ?`)) {
+      console.log("Delete", person)
+      removePerson(person)
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Message message={message} />
+      <Input text={"filter shown with: "} value={newFilterByName} onChange={handleFilterByNameChange} />
+      <h3>Add a new</h3>
+      <FormAddPerson onSubmit={addPerson} newNameValue={newName} newNameOnChange={handleNameChange} newNumberValue={newNumber} newNumberOnChange={handleNumberChange} />
+      <h3>Numbers</h3>
+      <Persons persons={persons} filter={newFilterByName} handleRemoveClick={handleRemoveClick} />
+    </div>
+  )
+
+}
+
+export default App
---
+++ osa3/puhelinluettelo/src/services/persons.js
@@ -0,0 +1,38 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/api/persons'
+
+const getAll = () => {
+    const request = axios.get(baseUrl)
+    return request.then(response => {
+        return response.data
+    })
+}
+
+const create = newObject => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => {
+        return response.data
+    })
+}
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => {
+        return response.data
+    })
+}
+
+const remove = (id) => {
+    const request = axios.delete(`${baseUrl}/${id}`)
+    return request.then(response => {
+        console.log("Remove", response.status)
+        return response.status
+    })
+}
+
+export default { 
+  getAll: getAll, 
+  create: create, 
+  update: update,
+  remove: remove 
+}
